fe3840a8ed52a572ca6bdc531f8963c1cdc3f362
Yoadev <yclicourt@gmail.com>
2025-03-12 19:14:25 -0400

    fix: add parameters whilelist en GlobalPipes and cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.enableCors({
+    origin:'http://localhost:3000'
+  })
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
