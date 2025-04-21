81169c16630cf637bc8631f10dfb107ab86c6ed9
Yoadev <yclicourt@gmail.com>
2025-04-20 23:17:06 -0400

    feat: Add service AddItemToCart, pending complete this enpoint

--- src/modules/cart/dto/create-cart.dto.ts
+++ src/modules/cart/dto/create-cart.dto.ts
@@ -1,23 +1,26 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { CartItemDto } from './cart-item.dto';
 
 export class CreateCartDto {
   @IsNumber()
   @IsNotEmpty()
-  productId: number;
-
-  @IsNumber()
-  @IsNotEmpty()
-  paymentId: number;
-
-  @IsNumber()
-  @IsNotEmpty()
-  method_sendId: number;
-
-  @IsNumber()
-  @IsNotEmpty()
   userId: number;
-
   @IsNumber()
   @IsNotEmpty()
   orderId: number;
+  @IsNotEmpty()
+  @IsArray()
+  items: CartItemDto[];
+  @IsNumber()
+  @IsNotEmpty()
+  discountAmount: number;
+  @IsNumber()
+  @IsNotEmpty()
+  taxAmount: number;
+  @IsNumber()
+  @IsNotEmpty()
+  shippingAmount: number;
+  @IsString()
+  @IsNotEmpty()
+  currency: string;
 }
--- src/modules/cart/dto/update-cart.dto.ts
+++ src/modules/cart/dto/update-cart.dto.ts
@@ -1,25 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCartDto } from './create-cart.dto';
-import { IsNumber, IsOptional } from 'class-validator';
 
-export class UpdateCartDto extends PartialType(CreateCartDto) {
-  @IsNumber()
-  @IsOptional()
-  productId: number | undefined;
-
-  @IsNumber()
-  @IsOptional()
-  paymentId?: number | undefined;
-
-  @IsNumber()
-  @IsOptional()
-  method_sentId?: number | undefined;
-
-  @IsNumber()
-  @IsOptional()
-  userId?: number | undefined;
-
-  @IsNumber()
-  @IsOptional()
-  orderId?: number | undefined;
-}
+export class UpdateCartDto extends PartialType(CreateCartDto) {}
